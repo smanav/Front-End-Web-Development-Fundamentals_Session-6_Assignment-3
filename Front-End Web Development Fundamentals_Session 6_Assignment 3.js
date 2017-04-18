e7308b22a0a8832c437407b1205262421b0ddc0a
smanav <smanav1991@gmail.com>
2017-04-18 16:14:23 +0530

    Add files via upload

---
+++ Front-End Web Development Fundamentals_Session 6_Assignment 3.js
@@ -0,0 +1,19 @@
+                // Here is the example of sum of numbers using javascript
+
+                // Sum using for loop
+                
+                var sum = 0;
+				for (var i = 1; i <= 1000; i++) {
+				   sum = sum + i;
+				}
+				console.log("Sum using for loop = " + sum);
+
+                //Sum using while loop
+
+                var sum = 0;
+				var number = 1;
+				while (number <= 1000) {  
+				  sum += number;        
+				  number++;           
+				}
+				console.log("Sum using while loop = " + sum); 
